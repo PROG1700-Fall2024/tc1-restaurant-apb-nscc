8c5aae59ec7f1fa16da7dc651a03aa3cb1a0dcc7
apb-nscc <W0487099@campus.nscc.ca>
2024-09-26 11:26:55 -0300

    Tech Check Done, Ready for Marking

--- TechCheck1-RestaurantBill.py
+++ TechCheck1-RestaurantBill.py
@@ -11,13 +11,47 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    ### Introduce User to the Application
+    print("Welcome to the Bill Calculator\n")
 
+    ### Assign Variables
+    BILL_HARDCODED = 85
+    TAX_RATE = 0.15
+    TIP_RATE = 0.2
 
+    ### Ask User if they want to use the program normally or use hard-coded values
+    mode = int(input("WOULD YOU LIKE TO RUN THIS PROGRAM NORMALLY OR WITH PRE-DETERMINED VALUES(TESTING)?\n 1 - Default \n 2 - Testing\n Input: "))
+    print("")
 
+    ### (OPTION 1 ONLY) - Enter bill amount
+    if mode == 1:
+        bill_amount = float(input("Please enter your original bill amount: "))
 
+    ### Display bill amount
+    bill_update = 0
 
+    if mode == 1 or mode == 2:
+        match mode:
+            case 1:
+                bill_update = bill_amount
+            case 2:
+                bill_update = BILL_HARDCODED
+    else:
+        print("Invalid answer, program ended.")
+    print("Your original bill amount is: ${0:.2f}".format(bill_update))
 
+    ### Display your tax amount
+    tax_value = bill_update * TAX_RATE
+    print("Your tax is: {0:.2f}%".format(tax_value))
 
+    ### Display your tip amount
+    tip_value = bill_update * TIP_RATE
+    print("Your tip is: {0:.2f}%".format(tip_value))
+
+    ### Display the bill total amount 
+    total_bill = bill_update + tax_value + tip_value
+    print("Your total is: ${0:.2f}".format(total_bill))
+    print("")
     # YOUR CODE ENDS HERE
 
 main()
